6aaf992648a278ca75de3afc8feb351570b102b7
dinhbaohkht <dinhbaohkht@gmail.com>
2023-09-23 11:09:59 +0700

    update app.py 23/9/2023

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ import json
 
 
 # Định nghĩa độ dài tối đa của hàng đợi khung hình
-frame_buffer_size = 100
+frame_buffer_size = 20
 
 frame_list = []  
 
@@ -44,17 +44,16 @@ db = firestore.client()
 
 firebase = pyrebase.initialize_app(config)
 storage = firebase.storage()
-licenplate = "38A3326"
-local = "camera Máy Bảo"
-violatedTime = datetime.now().strftime('%Y-%m-%d %H_%M_%S.%f')[:-2]
-violation = 'sai làn đường'
-videoName = violatedTime + " Sai làn đường " + licenplate +" "+ local
 
 #Read data
 
 #geting a document with a known ID 
 try :
-    result = db.collection('cameraIp').document("7OcuvIELFU7IYGnlBN4S").get()
+    docs = db.collection('cameraIp').where("namecam", "==", "Cam test Jetson").get()
+    for doc in docs:
+        ID = doc.id
+    result = db.collection('cameraIp').document(ID).get()
+    #result = db.collection('cameraIp').document("GXuLEZmIkLUWLPCff4pa").get()
 
     # if result.exists:
     #     print(result.to_dict())
@@ -98,8 +97,7 @@ try :
 
     # Đường dẫn đến tệp config.json
     config_file_path = 'config.json'
-
-    # Kiểm tra nếu tệp config.json đã tồn tại, thì xóa nó
+# Kiểm tra nếu tệp config.json đã tồn tại, thì xóa nó
     if os.path.exists(config_file_path):
         os.remove(config_file_path)
         print("Đã xóa tệp config.json")
@@ -131,7 +129,7 @@ def updateCrossLight(img, x0, y0, track_id, time_stamp):
     cv2.imwrite(image_path, img)
 
     data = {
-	    'licenplate':licenplate,
+	    'licenseplate':licenseplate,
 	    'local':local,
 	    'time':violatedTime, 
 	    'video_name':videoName, 
@@ -140,43 +138,62 @@ def updateCrossLight(img, x0, y0, track_id, time_stamp):
     db.collection('violation').add(data)
     # firebaseVideoPath = "Video/" + videoName +'.mp4'
     # storage.child(firebaseVideoPath).put("1.mp4","mp4")
+    storage.child("Violation_image/"+videoName+".jpg").put(image_path)
 	#
    
 # use path for library and engine file
 model = YoloTRT(library="yolov5/build/libmyplugins.so", engine="yolov5/build/yolov5s.engine", conf=0.5, yolo_ver="v5")
-cap = cv2.VideoCapture("/home/ctarg_lab_1/Desktop/video_output.mp4")
+
+
 #cap = cv2.VideoCapture("rtsp://admin:Admin@123@27.72.149.50:1554/profile3/media.smp") # open one video
+cap = cv2.VideoCapture("/home/ctarg_lab_1/Desktop/test10p.mp4")
 torch.cuda.empty_cache()
 time_list =[]
 stt = 0
 output_filename = 'output_video.mp4'
+#output_filename = str(violatedTime)[0:19] + ".mp4"
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 fps = int(cap.get(5))
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-out = cv2.VideoWriter(output_filename, fourcc, fps, (frame_width, frame_height))
+#fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+#out = cv2.VideoWriter(output_filename, fourcc, fps, (frame_width, frame_height))
+frame_number = 0
+if cap.read()[0]==False:
+    print("Camera is disconnected")
+    cap = cv2.VideoCapture("/home/ctarg_lab_1/Desktop/test10p.mp4")
 while True:
     box=[]
     classes = []
     score = []
     ret, frame = cap.read()
     if ret:
-        # Thêm khung hình hiện tại vào list
-        frame_list.append(frame)
-            # Loại bỏ khung hình cũ nhất nếu độ dài của danh sách vượt quá giới hạn tối đa
-        if len(frame_list) > frame_buffer_size:
-            frame_list.pop(0)  # Loại bỏ khung hình cũ nhất
+        
         pass
     else:
-        break
-    # 
+        #print('camera is disconected')
+        
+        #cap = cv2.VideoCapture("/home/ctarg_lab_1/Desktop/test10p.mp4")
+        #ret, frame = cap.read()
+        break 
+    frame_number+=1
+    licenseplate = "38A3326"
+    local = "camera test Jetson"
+    violatedTime = datetime.now()
+    violation = 'vuot den'
+    videoName = (str(violatedTime)[0:19] + "_" + violation +"_"+ licenseplate +"_"+ local).replace(":","_").replace(" ","_").replace("-","_")
+    # Thêm khung hình hiện tại vào list
+    frame_list.append(frame)
+    print("number_frame : ",len(frame_list))
+    # Loại bỏ khung hình cũ nhất nếu độ dài của danh sách vượt quá giới hạn tối đa
+    if len(frame_list) > frame_buffer_size:
+       frame_list.pop(0)  # Loại bỏ khung hình cũ nhất
     boxes, totalInference, infernceTime1, infernceTime2, infernceTime3 = model.Inference(frame)
     t_track = datetime.now()
     if len(boxes):
         boxes = tracker.update(boxes,frame)
        
     t_track = datetime.now() - t_track
-    frame = tracker.draw_bboxes(frame, boxes, None)
+    #frame = tracker.draw_bboxes(frame, boxes, None)
     t_error = datetime.now()
     current_color = colorDetector(frame)
     time_stamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
@@ -190,31 +207,31 @@ while True:
         y1 = int(box[3])
         current[track_id]  = midPoint(x0,y0,x1,y1)
         if track_id in previous:
-            cv2.line(frame, previous[track_id], current[track_id], (0,255,0), 1)
+            #cv2.line(frame, previous[track_id], current[track_id], (0,255,0), 1)
             line_group0 = [lane_left, lane_center, lane_right]
             for element in line_group0:
                 if len(element):
-                    start_line = element[0],element[1]
-                    end_line = element[2], element[3]
-                    if intersect(previous[track_id],current[track_id], start_line, end_line):
-                        print(intersect(previous[track_id],current[track_id], start_line, end_line))
-                        if line_group0.index(element) == 1:
+                    if line_group0.index(element) == 1 : #and current_color == "red":
+                        start_line = element[0],element[1]
+                        end_line = element[2], element[3]
+                        if intersect(previous[track_id],current[track_id], start_line, end_line):
+                            print(intersect(previous[track_id],current[track_id], start_line, end_line))
                             t_counter1.append(track_id)
-                            """
-                            if current_color != "red":
-                                print("Fined")
-                                updateCrossLight(frame, x0, y0, track_id, time_stamp)
-                            """
                             print("Fined")
+                            cv2.rectangle(frame, (x0, y0), (x1, y1), (0, 0, 255), 2)
                             updateCrossLight(frame, x0, y0, track_id, time_stamp)
-
+                            if os.path.exists("output_video.mp4"):
+                                os.remove("output_video.mp4")
+                            fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+                            out = cv2.VideoWriter(output_filename, fourcc, fps, (frame_width, frame_height))
                             for f in frame_list:
-                                print("write")
+                                #print("write")
                                 out.write(f)
 
                             out.release()
                             firebaseVideoPath = "Video/" + videoName +'.mp4'
-                            storage.child(firebaseVideoPath).put("output_video.mp4","mp4")
+                            storage.child(firebaseVideoPath).put('output_video.mp4',"mp4")
+
         previous[track_id] = current[track_id]
     #t_error = datetime.now() - t_error
     print("Total FPS: {} sec".format(1/totalInference))
